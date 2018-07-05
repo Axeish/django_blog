48ba7ec4ba0df48091fa8313e4c20d6521874f0e
Ashish Kumar <kumar.as@husky.neu.edu>
2018-07-05 12:30:40 -0700

    edit, delete, search options implemented

--- posts/models.py
+++ posts/models.py
@@ -3,11 +3,15 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.core.urlresolvers import reverse
+from django.db.models.signals import pre_save
+from django.utils.text import slugify
 # Create your models here.
 
 
 class Post(models.Model):
+
 	title = models.CharField(max_length=120)
+	slug = models.SlugField(unique=True)
 	content = models.TextField()
 	updated = models.DateTimeField(auto_now= True, auto_now_add= False)
 	timestamp =models.DateTimeField(auto_now= False, auto_now_add=True)
@@ -22,3 +26,22 @@ class Post(models.Model):
 		
 	def get_absolute_url(self):
 	    return reverse("detail",kwargs = {"id":self.id})
+
+def create_slug(instance, new_slug=None):
+	slug = slugify(instance.title)
+	if new_slug is not None:
+		slug= new_slug
+
+	qs = Post.objects.filter(slug=slug).order_by("-id")
+	exists = qs.exists()
+	if exists:
+		new_slug = "%s-%s" %(slug,gs.first().id)
+		return create_slug(instance,new_slug=new_slug)		
+	return slug
+
+
+def pre_save_posts_resolver(sender, instance, *args, **kwargs ):
+	if not instance.slug:	
+	    instance.slug = create_slug(instance)
+			
+pre_save.connect(pre_save_posts_resolver, sender=Post)	        
--- posts/views.py
+++ posts/views.py
@@ -3,6 +3,9 @@ from __future__ import unicode_literals
 from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.shortcuts import render
+from django.db.models import Q
 
 # Create your views here.
 from .models import Post
@@ -44,12 +47,33 @@ def post_delete(request,id=None):
 	return redirect("list")
 
 def post_list(request):
-	queryset= Post.objects.all()
+	queryset_list= Post.objects.all().order_by("-timestamp")
+	
+	query= request.GET.get('q')
+	print ("hi XXXXXXXXXXXXXXXXXXXX ")
+	print(query)
+	if query:
+		queryset_list = queryset_list.filter(title__icontains=query) 
+		print(queryset_list)
+		print(len(queryset_list))
+	paginator = Paginator(queryset_list, 5) # Show 25 contacts per page		
+	page = request.GET.get('page')
+	try:
+		queryset = paginator.page(page)
+	except PageNotAnInteger:
+		queryset = paginator.page(1)
+	except EmptyPage:
+		queryset = paginator.page(	paginator.num_pages)	
 	context = {
 	"title":"list",
 	"object_list":queryset,
 	}
-	return render(request,"index.html",context)
+	return render(request,"post_list.html",context)
+
+
+
+
+
 def post_detail(request,id=None):
 	instance = get_object_or_404(Post, id=id)
 	context = {
